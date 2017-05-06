31e587678deb7b159254819cd5a8d60a58ccef76
Erik Hallmark <erikhallmarkdev@gmail.com>
2017-05-05 18:56:04 -0700

    Added status to all loaders

--- indeterminateprogressbar.py
+++ indeterminateprogressbar.py
@@ -1,6 +1,6 @@
 import sys
 
-def indetermineate_progressbar():
+def indetermineate_progressbar(status = ""):
     indetermineate_progressbar.current += 1;
     bar_length = 60;
 
@@ -9,7 +9,7 @@ def indetermineate_progressbar():
     start = i
     end = bar_length - i - 1
 ''
-    bar = "[" + "." * start + " " * end + "]\r"
+    bar = "[" + "." * start + " " * end + "]" + status  + "\r"
 
     sys.stdout.write(bar)
     sys.stdout.flush()
--- spinner.py
+++ spinner.py
@@ -1,10 +1,10 @@
 import sys
 
-def spinner():
+def spinner(status = ""):
     spinner.current += 1
     chars = ['/', '-', '\\', '|']
     i = spinner.current % len(chars)
-    sys.stdout.write(chars[i] + "\r")
+    sys.stdout.write(chars[i] + status  + "\r")
     sys.stdout.flush()
 
 spinner.current = 0
